1a51ea1971f9dd18dd634942e63619cb6ab240dc
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-12-12 23:37:14 +0900

    make dataset for myself

--- easy_task/data.py
+++ easy_task/data.py
@@ -15,57 +15,35 @@ from tqdm import tqdm
 import pandas as pd
 import argparse
 import h5py
-from sklearn.model_selection import train_test_split
+import glob
+import make_data
+# from sklearn.model_selection import train_test_split
 class LoadDataset(Dataset):
-    def __init__(self, dir, time = 100, width = 128, height = 128):
+    def __init__(self, train:bool, dir):
         self.dir = dir
-        #h5ファイルのディレクトリのリスト
-        self.dir_h5 = []
-        self.width = width
-        self.height = height
-        self.time = time
-        for _, _, files in os.walk(self.dir):
-            for file in files:
-                if file.endswith('.h5'):
-                    self.dir_h5.append(os.path.join(self.dir, file)) 
-
-        "テストデータ"
-        self.divide = int((len(self.dir_h5)*0.8))
-        if which == "train":
-            self.dir_h5 = self.dir_h5[:self.divide]
-        elif which == "test":
-            self.dir_h5 = self.dir_h5[self.divide:]
+        self.all_files = glob.glob(f"{self.dir}/*")
+        self.divide = int((len(self.all_files)*0.8))
+        if train:
+            self.file_lst = self.all_files[:self.divide]
         else:
-            print("error by data.py")
-            exit()
-        self.index = len(self.dir_h5)
-        
+            self.file_lst = self.all_files[self.divide:]
     def __len__(self):
-        return self.index
+        return len(self.file_lst)
 
     def __getitem__(self, index):
-        events = torch.zeros(2, self.time, self.width, self.height)
-        with h5py.File(self.dir_h5[index], "r") as f:
+        with h5py.File(self.file_lst[index], "r") as f:
             label = f['label'][()]
-
-            self.events_ = f['events'][()]
-            for i_, i in enumerate(self.events_):
-                ###i(h5ファイルから読み込まれるデータ):(timestep, y?, x?, pol)
-                if i[0] >= self.time:
-                    break
-                # print(i)
-                events[ i[3], i[0], i[2], i[1]] = 1
-                ###events:(pol, time, x, y)
-                # print("asdfasdadassdfa")
-        return events, label
+            input = f['input'][()]
+        input = torch.from_numpy(input.astype(np.float32)).clone()
+        return input, label
 
 if __name__ == "__main__":
-    a = LoadDataset('C:/Users/oosim/Desktop/snn/v2e/output/', time = 20)
-    ru = a.__getitem__(2)
-    kazu = torch.count_nonzero(ru[0] == 1.)
-    print(f'kazu:{kazu}')
-    print('1111111111')
-    print(a.dir_h5[0])
-    print(ru)
-    print(ru[0].shape)
+    dataset_path = "dataset/"
+    youtube_path = "gomibako/h5.gif"
+    a = LoadDataset(dir=dataset_path, train=True)
+    input, label = a[6]
+    print(input)
+    print(label)
+    print(input.shape[0])
+    make_data.youtube(input, youtube_path)
     
