9f6a1cdf8a91dad0381a11c966fdfa20edf76a27
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-12-12 19:31:53 +0900

    ディレクトリを簡単に管理できるようにした

--- easy_task/make_data.py
+++ easy_task/make_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 import torchvision
 import random
 import pandas as pd
-import os
+import os, shutil
 
 
 def draw_circle(center, radius,time=10, pixel=128):
@@ -23,10 +23,10 @@ def youtube(events, path):
     images[0].save(path, duration = 100, save_all=True, append_images=images[1:], loop = 50)
 
 
-def SaveEvents(path, ebents, label):
+def SaveEvents(path, events, label):
     with h5py.File(path, 'w') as f:
         f.create_dataset('label', data = label)
-        f.create_dataset('input', data = ebents)
+        f.create_dataset('input', data = events)
     return
 
 
@@ -39,14 +39,22 @@ if __name__ == "__main__":
     radius_list, x_list, y_list = [],[],[]
     label_list = []
     path_list = []
+    youtube_dir = "youtube"
+    dataset_dir = "dataset"
+    csv_path = f"dataset/info.csv"
 
-
-
+    if os.path.exists(youtube_dir):
+        shutil.rmtree(youtube_dir)
+        os.makedirs(youtube_dir)
+    if os.path.exists(dataset_dir):
+        shutil.rmtree(dataset_dir)
+        os.makedirs(dataset_dir)
     for i in range(number_of_data):
         x, y, r, label = None, None, None, 0
-        youtube_path = f"youtube/{str(i).zfill(4)}.gif"
-        h5py_path = f"dataset/{str(i).zfill(4)}.h5"
-        csv_path = f"dataset/info.csv"
+        youtube_path = os.path.join(youtube_dir, f"{str(i).zfill(4)}.gif")
+        dataset_path = os.path.join(dataset_dir, f"{str(i).zfill(4)}.h5")
+
+
         events = torch.where(torch.rand(time, pixel, pixel)<= noize_rate, 1, 0)
         if random.random() <= 0.5:
             x = random.randint(0,pixel)
@@ -61,13 +69,13 @@ if __name__ == "__main__":
                 events[t,:,:] = torch.where(events[t,:,:] == True, 1, 0)
         
         youtube(events, youtube_path)
-        SaveEvents(h5py_path, events, label)
+        SaveEvents(dataset_path, events, label)
         numer_list.append(i)
         radius_list.append(r)
         x_list.append(x)
         y_list.append(y)
         label_list.append(label)
-        path_list.append(h5py_path)
+        path_list.append(dataset_path)
     
     df = pd.DataFrame(
         data={
