7a9b084ef263ca5936ae693f18912f7b96a6c260
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-01-11 17:21:08 +0900

    aaa

--- easy_task/segmentation/an.py
+++ easy_task/segmentation/an.py
@@ -1,4 +1,12 @@
-import numpy as np
-a = np.zeros((2,2))
-a[1,1] = 1
-print(a)
+# import torch
+# import torch.nn as nn
+# c = nn.MSELoss()
+# a = torch.zeros((2,3))
+# b = torch.ones((2,3))
+# print(b * 5)
+
+import torch
+a = torch.tensor([1.234, 2.345, 3.456], dtype=torch.float)
+b = torch.floor(a) # 1, 2, 3になる
+c = torch.ceil(a)  # 2, 3, 4になる
+print(b, type(b), b.dtype)
