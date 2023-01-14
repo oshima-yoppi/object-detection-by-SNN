67e4d1ee0eb72bece7f8060595b05b53fd6d739a
YoshiyaOsm <oosima@kimura-lab.net>
2023-01-14 22:26:32 +0900

    for ipynb

--- easy_task/segmentation/an.py
+++ easy_task/segmentation/an.py
@@ -1,10 +1,11 @@
+#%%
 # import torch
 # import torch.nn as nn
 # c = nn.MSELoss()
 # a = torch.zeros((2,3))
 # b = torch.ones((2,3))
 # print(b * 5)
+a = [i for i in range(5)]
+a
 
-import torch
-a = torch.rand((2,3))
-print(torch.sigmoid(a))
+# %%
