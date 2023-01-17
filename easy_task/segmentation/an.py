0df6f0cbf50c4485bf8c34206d82b07886dcacb7
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-01-17 23:09:34 +0900

    error check

--- easy_task/segmentation/an.py
+++ easy_task/segmentation/an.py
@@ -9,3 +9,11 @@ a = [i for i in range(5)]
 a
 
 # %%
+try:
+    a = b
+except Exception as e:
+    import traceback
+    print('--------error--------')
+    traceback.print_exc()
+    print('--------error--------')
+print(99999)
