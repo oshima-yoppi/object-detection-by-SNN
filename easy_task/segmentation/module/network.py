7482ac8ac4abf5c0c4c79f2246c7c7c5175df241
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-01-17 14:47:45 +0900

    clean code

--- easy_task/segmentation/module/network.py
+++ easy_task/segmentation/module/network.py
@@ -26,22 +26,7 @@ from IPython.display import HTML
 
 
 
-# spike_grad = surrogate.atan()
-def cnn(beta, spike_grad):
-    reshape_bool = False
-    net = nn.Sequential(nn.Conv2d(1, 12, 5),
-                    nn.MaxPool2d(2),
-                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
-                    nn.Conv2d(12, 32, 5),
-                    nn.MaxPool2d(2),
-                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
-                    nn.Flatten(),
-                    nn.Linear(5408, 64*64),
-                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True),
-                    # nn.Linear(1024, 2),
-                    # snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
-                    )
-    return net, reshape_bool
+
 class BaseFunction:
     def __call__(self, data):
         soft = nn.Softmax2d()
@@ -60,25 +45,65 @@ class BaseFunction:
         # print(spk_rec.shape)
         spk_cnt = compute_loss.spike_count(spk_rec, channel=True)# batch channel(n_class) pixel pixel 
         spk_cnt_ = spk_cnt[0,0,:,:].reshape(self.pixel, self.pixel).to('cpu').detach().numpy().copy()
-        # plt.figure()
-        # plt.imshow(spk_cnt_)
-        # plt.show()
-        print(np.sum(spk_cnt_.reshape(-1)))
-        # pred_pro = soft(spk_cnt)
+        
+        # print(np.sum(spk_cnt_.reshape(-1)))
+       
         pred_pro = F.softmax(spk_cnt, dim=1)
         pred_pro_ = pred_pro[0,0,:,:].reshape(self.pixel, self.pixel).to('cpu').detach().numpy().copy()
         pred_pro__ = pred_pro[0,1,:,:].reshape(self.pixel, self.pixel).to('cpu').detach().numpy().copy()
         
-        # plt.figure()
-        # ax1 = plt.subplot(1,2,1)
-        # ax2 = plt.subplot(1,2,2)
-        # ax1.imshow(pred_pro_)
-        # ax1.set_title('not safe')
-        # ax2.imshow(pred_pro__)
-        # ax2.set_title('safe')
-        # plt.show()
+       
         return pred_pro
 class ConvDense1(BaseFunction):
+    """
+    
+    """
+    def __init__(self, beta, spike_grad, device,pixel=64,  reshape_bool = True,parm_lean=True, ):
+        self.parm_learn = True
+        self.reshape_bool = reshape_bool
+        self.pixel = pixel
+        c0 = 1
+        c1 = 16
+        c2 = 16
+        n_class=2
+        encode_kernel = 3
+        decode_kernel = 3
+        n_neuron = 4096
+        self.network = nn.Sequential(
+            nn.Conv2d(c0, c2, encode_kernel, padding=encode_kernel//2),
+            nn.MaxPool2d(2, stride=2),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, learn_beta=parm_lean, learn_threshold=parm_lean),
+            
+
+            # nn.Conv2d(c1, c2, encode_kernel, padding=encode_kernel//2),
+            # nn.MaxPool2d(2, stride=2),
+            # snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+            
+            nn.Conv2d(c2, c2, encode_kernel, padding=encode_kernel//2),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, learn_beta=parm_lean, learn_threshold=parm_lean),
+            
+            # nn.Upsample(scale_factor=2),
+            # nn.Conv2d(c2, c1, decode_kernel, padding=encode_kernel//2),
+            # snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+            
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(c2, c0, decode_kernel, padding=decode_kernel//2),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, learn_beta=parm_lean, learn_threshold=parm_lean),
+            
+            
+            nn.Conv2d(c0, n_class, 1,),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, learn_beta=parm_lean, learn_threshold=parm_lean),
+            
+            nn.Flatten(),
+            nn.Linear(pixel*pixel*2, n_neuron),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, learn_beta=parm_lean, learn_threshold=parm_lean),
+            
+            nn.Linear(n_neuron, pixel*pixel*2),
+            # nn.Linear(pixel*pixel*2, pixel*pixel*2),
+            snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output = True,learn_beta=parm_lean, learn_threshold=parm_lean),
+        ).to(device)
+    
+class ConvDense0(BaseFunction):
     def __init__(self, beta, spike_grad, device,pixel=64,  reshape_bool = True,parm_lean=True, ):
         self.parm_learn = True
         self.reshape_bool = reshape_bool
@@ -123,9 +148,22 @@ class ConvDense1(BaseFunction):
             nn.Linear(pixel*pixel*2, pixel*pixel*2),
             snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output = True,learn_beta=parm_lean, learn_threshold=parm_lean),
         ).to(device)
-    
-
-
+# spike_grad = surrogate.atan()
+def cnn(beta, spike_grad):
+    reshape_bool = False
+    net = nn.Sequential(nn.Conv2d(1, 12, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Conv2d(12, 32, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Flatten(),
+                    nn.Linear(5408, 64*64),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True),
+                    # nn.Linear(1024, 2),
+                    # snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+                    )
+    return net, reshape_bool
 
 def ConvDense(beta, spike_grad,pixel=64, parm_lean=True):
     c0 = 1
