db65e051daf1df260ccec026fb90ea43de5cfa5c
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-01-20 15:04:54 +0900

    chekc

--- easy_task/segmentation/an.py
+++ easy_task/segmentation/an.py
@@ -1,19 +1,3 @@
-#%%
-# import torch
-# import torch.nn as nn
-# c = nn.MSELoss()
-# a = torch.zeros((2,3))
-# b = torch.ones((2,3))
-# print(b * 5)
-a = [i for i in range(5)]
-a
-
-# %%
-try:
-    a = b
-except Exception as e:
-    import traceback
-    print('--------error--------')
-    traceback.print_exc()
-    print('--------error--------')
-print(99999)
+a = 0
+print(a)
+a = 2
