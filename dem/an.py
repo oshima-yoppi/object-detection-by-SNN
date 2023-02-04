42b95883dccf37552b7cc3a9722e3795e2e98dc7
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-02-04 16:29:40 +0900

    easy

--- dem/an.py
+++ dem/an.py
@@ -38,43 +38,33 @@ def resize_bilinear(src, hd, wd):
 
     return dst
 
-def img2plot(path, all_pixel, partial_pixel, reshape):
-    data = np.fromfile(path, dtype='>i2')
-    data = data.reshape(all_pixel, all_pixel)
-    data = data[:partial_pixel, :partial_pixel]
-    # reshape = 100
-    data = resize_bilinear(data, reshape, reshape)
-    max_alt = data.max()
-    bias = data[0,0]
+def img2plot(data):
+    # data = np.load(path)
+    pix, pix = data.shape
     verts = []
-    for x in range(reshape):
-        for y in range(reshape):
-            verts.append(mathutils.Vector([x*meter_pixel, y*meter_pixel, RATE*(data[x, y] - bias)]))
+    for x in range(pix):
+        for y in range(pix):
+            verts.append(mathutils.Vector([x, y, (data[x, y])]))
             # verts.append(mathutils.Vector([np.uint8(x), np.uint8(y), np.uint8(data[x, y])]))
     return verts
 
 
 start = time.time()
-#path = "C:/Users/oosim/Desktop/object-detection-by-SNN/dem/blender/kaguya/1.img"
-path = "//kaguya/0.img" # 相対パスはスラッシュ二つにする必要があるらしい
-path = "//kaguya/1.img" # 相対パスはスラッシュ二つにする必要があるらしい
+path = "//kaguya/mat_make1.npy" # 相対パスはスラッシュ二つにする必要があるらしい
 path = bpy.path.abspath(path)
-RATE = 0.001
-pixel = 12288
-partial_pixel = 4000
-reshape = 4000
-meter_pixel = 7.403
-meter_pixel *= RATE
-verts = img2plot(path, pixel, partial_pixel, reshape)
+
+data = np.load(path)
+pixel,_ = data.shape
+
+verts = img2plot(path)
 fIndexes = []  # 面のリスト
 
-for x in range(0, reshape - 1):
-    for y in range(0, reshape - 1):
-        fIndexes.append([x + y * reshape,
-                         x + 1 + y * reshape,
-                         x + 1 + (y + 1) * reshape,
-                         x + (y + 1) * partial_pixel])
-
+for x in range(0, pixel - 1):
+    for y in range(0, pixel - 1):
+        fIndexes.append([x + y * pixel,
+                         x + 1 + y * pixel,
+                         x + 1 + (y + 1) * pixel,
+                         x + (y + 1) * pixel])
 
 mesh = bpy.data.meshes.new('wave')
 mesh.from_pydata(verts, [], fIndexes)  # 点と面の情報からメッシュを生成
@@ -82,4 +72,5 @@ mesh.from_pydata(verts, [], fIndexes)  # 点と面の情報からメッシュを
 obj = bpy.data.objects.new('wave', mesh)  # メッシュ情報を新規オブジェクトに渡す
 bpy.context.scene.collection.objects.link(obj)  # オブジェクトをシーン上にリンク(v2.8)
 print(time.time() - start)
+# bpy.context.scene.objects.link(obj) #v2.7
 obj.select = True  # 作ったオブジェクトを選択状態に
