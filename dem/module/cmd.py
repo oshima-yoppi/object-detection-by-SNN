0eb7a9558b8b3f033a9409f9efe1a5af965e78bb
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-02-07 01:43:54 +0900

    AONY

--- dem/module/cmd.py
+++ dem/module/cmd.py
@@ -1,5 +1,5 @@
 import os
 
 def v2e_cmd(number):
-    cmd = f"python ../v2e-master/v2e.py -o data --overwrite --output_file_path data/{number}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_exposure duration 0.05 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
+    cmd = f"python ../v2e-master/v2e.py -i blender/video/{number}.avi -o data --overwrite --output_file_path data/{number}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_exposure duration 0.1 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
     os.system(cmd)
--- dem/module/const.py
+++ dem/module/const.py
@@ -5,3 +5,6 @@ focal = 0.050# 焦点距離
 img_height, img_width = 240, 360 # カメラの大きさ
 sensor_height, sensor_width = 0.024, 0.036 # イメージセンサの大きさ 
 cam_x, cam_y, cam_z = 64, 64, 164 # カメラの初期位置
+
+# path
+# video_folder = 
