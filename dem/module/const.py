f3099ae3430febe24d116e7acc6f83946e6543b4
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-02-06 17:24:46 +0900

    定数関連ファイル

---
+++ dem/module/const.py
@@ -0,0 +1,7 @@
+# 設定ファイル。ここでいろんな変数を定義
+
+# blender
+focal = 0.050# 焦点距離 
+img_height, img_width = 240, 360 # カメラの大きさ
+sensor_height, sensor_width = 0.024, 0.036 # イメージセンサの大きさ 
+cam_x, cam_y, cam_z = 64, 64, 164 # カメラの初期位置
