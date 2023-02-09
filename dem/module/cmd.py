22aada5586fd9b5c383250e74f933e81bdeb90f1
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-02-09 13:21:22 +0900

    change small

--- dem/module/cmd.py
+++ dem/module/cmd.py
@@ -3,8 +3,8 @@ import os
 def v2e_cmd(number):
     cmd = f"python ../v2e-master/v2e.py -i blender/video/{number}.avi -o data --overwrite --output_file_path data/{number}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_exposure duration 0.1 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
 
-    number = str(number).zfill(5)
-    cmd = f"python ../v2e-master/v2e.py -i blender/video/{number}.avi -o data --overwrite --output_file_path data/{number}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
+    number_ = str(number).zfill(5)
+    cmd = f"python ../v2e-master/v2e.py -i blender/video/{number_}.avi -o data --overwrite --output_file_path data/{number_}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
 
 
     os.system(cmd)
