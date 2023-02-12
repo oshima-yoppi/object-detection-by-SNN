aa21e9ba587f5e082f7e608c64063bb0ed2e7706
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-02-12 14:53:48 +0900

    a

--- dem/module/cmd.py
+++ dem/module/cmd.py
@@ -4,7 +4,8 @@ def v2e_cmd(number):
 
 
     number_ = str(number).zfill(5)
-    cmd = f"python ../v2e-master/v2e.py -i blender/video/{number_}.avi -o {RAW_EVENT_PATH} --overwrite --output_file_path data/{number_}.h5  --disable --dvs346 --neg_thres 0.1 --pos_thres 0.1 --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
+    file_name = os.path.join(RAW_EVENT_PATH, f'{number_}.h5')
+    cmd = f"python ../v2e-master/v2e.py -i blender/video/{number_}.avi -o {RAW_EVENT_PATH} --overwrite --output_file_path {file_name}  --disable --dvs346 --neg_thres {EVENT_TH} --pos_thres {EVENT_TH} --dvs_aedat2 None --dvs_text None --skip_video_output --no_preview --dvs_h5 True"
 
 
     os.system(cmd)
