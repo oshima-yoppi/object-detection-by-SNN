fba8b27eb466ee32727c360de7698d0b93d88194
YoshiyaOsm <oosima@kimura-lab.net>
2023-05-15 22:11:50 +0900

    a

--- dem/blender_make_video.py
+++ dem/blender_make_video.py
@@ -38,7 +38,7 @@ def init(theta, save_dir):
     y_start = CAM_Y
     y_finish = y_start
     z_start = CAM_Z
-    z_finish = 2.5
+    z_finish = 5
     z_length = z_start - z_finish 
     velocity = 1 # 速度m/s
     video_fps =  100#int(velocity*frame_num/z_length)
@@ -135,7 +135,7 @@ if __name__ == "__main__":
 
     
     
-    DATA_NUM = 1
+    DATA_NUM = 3
     object_name = "dem"
     for i in range(DATA_NUM):
         number = str(i).zfill(5)
