414720f917e3a328db744388c2e0fb5b22d4e378
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2023-05-21 01:31:14 +0900

    mivce!!!!!!!!!!!!!

--- dem/experiment_all.py
+++ dem/experiment_all.py
@@ -3,7 +3,7 @@ import itertools
 
 soft_reset_lst = [True, False]
 parm_learn_lst = [True, False]
-finish_step_lst = [1 ,4, 8]
+finish_step_lst = [1 , 5, 9]
 combinations = list(itertools.product(soft_reset_lst, parm_learn_lst, finish_step_lst))
 
 for soft_reset, parm_learn, finish_step in combinations:
