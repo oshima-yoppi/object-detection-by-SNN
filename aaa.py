ed8a78d358e2a20553603b01c87cbeaf5f95163d
YoshiyaOsm <oosima@kimura-lab.net>
2023-07-15 00:12:17 +0900

    z

--- aaa.py
+++ aaa.py
@@ -1,7 +1,7 @@
 import os
 
-cmd = "python all.py --time 10 " 
+cmd = "python all.py --time 10 "
 os.system(cmd)
 
-cmd = "python all.py --time 1000 " 
+cmd = "python all.py --time 1000 "
 os.system(cmd)
